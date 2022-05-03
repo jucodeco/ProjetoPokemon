08b4122ab4b0652deacd9f9e27617b8901a10cfd
juliana <juju.codeck@gmail.com>
2022-05-03 11:02:46 -0300

    segundo commit

--- app/src/main/java/com/example/pokemon/ActivityLogin.kt
+++ app/src/main/java/com/example/pokemon/ActivityLogin.kt
@@ -3,14 +3,14 @@ package com.example.pokemon
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import com.example.pokemon.MainActivity
 
+
+@Suppress("UNREACHABLE_CODE")
 class ActivityLogin : AppCompatActivity() {
 
     private lateinit var editemail: EditText
@@ -25,17 +25,42 @@ class ActivityLogin : AppCompatActivity() {
         editemail = findViewById(R.id.editemail)
         editsenha = findViewById(R.id.editsenha)
         btentrar = findViewById(R.id.btentrar)
-
+        val pref = getPreferences(MODE_PRIVATE)
+        val email = pref.getString("EMAIL", null )
+        editemail.setText(email ?: "")
+        val senha = pref.getInt("SENHA", Int.MIN_VALUE)
+        editsenha.setText(if (senha == Int.MIN_VALUE) "" else senha.toString())
         btentrar.setOnClickListener {
             when {
                 editemail.text.toString().isEmpty() || editsenha.text.toString().isEmpty() ->
                     Toast.makeText(applicationContext, "Prencha os campos", Toast.LENGTH_LONG).show()
 
-                editemail.text.toString() == "juliana@gmail.com" && editsenha.text.toString() == "1234" ->
+                editemail.text.toString() == "jucodeco@gmail.com" && editsenha.text.toString() == "1234" -> {
+                    val editor = pref.edit()
+                    editor.putString("EMAIL", editemail.text.toString())
+                    editor.putInt("SENHA", editsenha.text.toString().toInt())
+                    editor.apply()
                     startActivity(Intent(applicationContext, MainActivity::class.java))
-
+                }
                 else -> Toast.makeText(applicationContext, "Login Incorreto", Toast.LENGTH_LONG).show()
             }
+
+
         }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
